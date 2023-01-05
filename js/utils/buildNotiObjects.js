0c70b7a1b5ba996a4f2ca79131485b0f9bc6f813
Alex Bojorquez <Alex.bojorquez2598@gmail.com>
2023-01-04 21:43:55 -0500

    created a function thats responsible for taking notification data from an api and turning them into the correct notification objects

---
+++ js/utils/buildNotiObjects.js
@@ -0,0 +1,29 @@
+import FollowNoti from "../classes/FollowNoti.js";
+import MessageNoti from "../classes/MessageNoti.js";
+import CommentNoti from "../classes/CommentNoti.js";
+import ReactNoti from "../classes/ReactNoti.js";
+
+/* 
+bound each constructor to a key so they can be accessed
+without using a long conditional
+
+takes in notification api data and turns them into objects
+*/
+
+const buildNotiObjects = (() => {
+  const typeMap = {
+    follow: FollowNoti,
+    message: MessageNoti,
+    comment: CommentNoti,
+    react: ReactNoti,
+  };
+
+  return function (notifications) {
+    return notifications.map((noti) => {
+      const notiConstructor = typeMap[noti.type];
+      return new notiConstructor(noti);
+    });
+  };
+})();
+
+export default buildNotiObjects;
